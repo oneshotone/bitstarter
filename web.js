fc1f95d2c4fd8ca0f6339ff530a22b00696eaefc
Kevin <ioneshotone@gmail.com>
2013-07-18 04:40:42 +0000

    update express

--- web.js
+++ web.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var routes = require('./routes');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 app.use(app.router);
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.use(express.bodyParser);
+app.use(express.methodOverride);
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', routes.index);
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
