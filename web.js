b3cc3db26d2923cb8b36aa14593c3819ebd32ec0
Kevin <ioneshotone@gmail.com>
2013-07-18 03:56:19 +0000

    first routes

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var routes = require('./app/controllers');
+var views = require('./app/views');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('It goes on and on my friends!');
-});
+app.use(app.router);
+app.get('/', routes.index);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
